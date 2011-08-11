8166b6063650d4726c7266f0d28c16998b58b149
Steve Coffee <scoffee@consultmore.com>
2011-08-11 13:59:50 -0500

    refactoring

--- Resources/app.js
+++ Resources/app.js
@@ -1,34 +1,7 @@
 //initialization
+Ti.include('modules/db.js');
 
-//db
-var db = Titanium.Database.open('mydb');
-
-//players(player_id, name)
-db.execute('Create table if not exists ' +
-	' Players(' +
-	' 	Player_ID integer primary key, ' +
-	' 	Name text)');
-
-//player_words(player_word_id, player_id, word, correct_count, incorrect_count)
-db.execute('Create table if not exists ' +
-	' Player_Words(' +
-	' 	Player_Word_ID integer primary key, ' +
-	' 	Player_ID integer, ' +
-	' 	Word text, ' +
-	' 	Correct_Count integer, ' +
-	' 	Incorrect_Count integer)');
-
-//player_word_choices(player_word_id, player_id, 
-//		correct_word, option_word, total_trial_count, chosen_count)
-db.execute('Create table if not exists ' +
-	' Player_Word_Choices(' +
-	' 	Player_Word_Choice_ID integer primary key, ' +
-	' 	Player_ID integer, ' +
-	' 	Correct_Word text, ' +
-	' 	Option_Word text, ' +
-	' 	Total_Trial_Count integer, ' +
-	' 	Chosen_Count integer)');
-
+db.initialize();
 
 //open main window
 var winMain = Titanium.UI.createWindow({  
--- Resources/modules/db.js
+++ Resources/modules/db.js
@@ -15,6 +15,33 @@ var db = (function () {
         return conn;
     } 
 
+	my.initialize = function () {
+		//players(player_id, name)
+		myConnection().execute('Create table if not exists ' +
+			' Players(' +
+			' 	Player_ID integer primary key, ' +
+			' 	Name text)');
+		
+		//player_words(player_word_id, player_id, word, correct_count, incorrect_count)
+		myConnection().execute('Create table if not exists ' +
+			' Player_Words(' +
+			' 	Player_Word_ID integer primary key, ' +
+			' 	Player_ID integer, ' +
+			' 	Word text, ' +
+			' 	Correct_Count integer, ' +
+			' 	Incorrect_Count integer)');
+		
+		//player_word_choices(player_word_id, player_id, 
+		//		correct_word, option_word, total_trial_count, chosen_count)
+		myConnection().execute('Create table if not exists ' +
+			' Player_Word_Choices(' +
+			' 	Player_Word_Choice_ID integer primary key, ' +
+			' 	Player_ID integer, ' +
+			' 	Correct_Word text, ' +
+			' 	Option_Word text, ' +
+			' 	Total_Trial_Count integer, ' +
+			' 	Chosen_Count integer)');
+	};
 	my.getPlayers = function () {
 		var rows, result = [];
 		
